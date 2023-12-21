e89ef7ef5ad346bf2bf6ae873e55fb8afc43d42c
Oleksii <alekseishevchenko8@gmail.com>
2023-12-21 21:24:29 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,86 @@
+contacts = {}
+
+def input_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except (KeyError, ValueError, IndexError) as e:
+            return f"Input error: {str(e)}"
+    return wrapper
+
+@input_error
+def add_contact(name, phone):
+    if name in contacts.keys():
+        raise ValueError(f"Contact {name} already exists")
+    
+    contacts[name] = phone
+    
+    return f"Contact {name} added with phone {phone}"
+
+@input_error
+def change_contact(name, phone):
+    if name not in contacts.keys():
+        raise KeyError(f"Contact {name} not found")
+        
+    contacts[name] = phone
+    return f"Phone number for contact {name} changed to {phone}"
+
+@input_error
+def get_phone(name):
+    if name not in contacts.keys():
+        raise KeyError(f"Contact {name} not found")
+        
+    return f"The phone number for {name} is {contacts[name]}"
+
+@input_error
+def show_all():
+    if not contacts:
+        return "No contacts available"
+    contacts_list = "\n".join([f"{name}: {phone}" for name, phone in contacts.items()])
+    return contacts_list
+
+@input_error
+def hello():
+    return "How can I help you?"
+
+@input_error
+def good_bye():
+    return "Good bye!"
+
+@input_error
+def get_handler(command):
+    handlers = {
+        "good bye": good_bye,
+        "close": good_bye,
+        "exit": good_bye,
+        "hello": hello,
+        "add": add_contact,
+        "change": change_contact,
+        "phone": get_phone,
+        "show all": show_all,
+    }
+
+    command_key, *args = command.split(" ")
+    for key, handler in handlers.items():
+        if key.startswith(command_key):
+            try:
+                return handler(*args)
+                
+            except TypeError:
+                return handler()
+
+    return "Invalid command. Please try again."
+
+def main():
+    
+    while True:
+        command = input("Enter a command: ").lower()
+        
+        if command in ["good bye", "close", "exit"]:
+            print(get_handler(command))
+            break
+            
+        print(get_handler(command))
+
+if __name__ == "__main__":
+    main()
